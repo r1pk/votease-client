f1ffdd4c56bab33fa5c2299065fa2f090ea5603a
Patryk Krawczyk <patrykekrawczyk@gmail.com>
2023-01-29 02:10:40 +0100

    Change default TextField variant to outlined

--- src/components/form/TextField.jsx
+++ src/components/form/TextField.jsx
@@ -3,7 +3,7 @@ import { forwardRef } from 'react';
 import { TextField as MUITextField } from '@mui/material';
 
 const TextField = forwardRef((props, ref) => {
-  return <MUITextField size="small" variant="standard" ref={ref} {...props} />;
+  return <MUITextField size="small" variant="outlined" ref={ref} {...props} />;
 });
 
 TextField.displayName = 'TextField';
